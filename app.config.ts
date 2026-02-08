6155559a7e73c74c1d9f6e8c99f9e8ebe2c42276
sayedabuelela <abuelela.dev@gmail.com>
2026-02-08 10:31:00 +0200

    POS card approved transactions FIN-10308

--- app.config.ts
+++ app.config.ts
@@ -4,7 +4,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
   name: 'Kashier',
   slug: 'kashier',
-  version: '1.6.3',
+  version: '2.0.0',
   orientation: 'portrait',
   icon: './src/shared/assets/images/app-icon.png',
   scheme: 'kashier',
